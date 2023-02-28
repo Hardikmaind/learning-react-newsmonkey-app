fac766089b60497c173c7f6002c02236a37c7c87
Hardikmaind <maindhardik@gmail.com>
2023-03-01 00:17:15 +0530

    vid-24 harry react course

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,20 +1,24 @@
-import React, { Component } from 'react'
-import Newsitem from './Newsitem'
+import React, { Component } from "react";
+import Newsitem from "./Newsitem";
 
-
-export default class News  extends Component {
+export default class News extends Component {
   render() {
     return (
-      <div>
-        this is a news component
-        <Newsitem/>     
-        <Newsitem/>     
-        <Newsitem/>     
-        <Newsitem/>     
-        <Newsitem/>     
-        <Newsitem/>     
+      <div className="container my-3">
+        <h2>newsmonkey top headlines</h2>
+        <div className="row">
+          {/* here col md is a class in bootsrap which says  in medium devices, 4 columns le lgi. bootstrap has 12 column grib in it. so in this way we get 4*3=12. so the each card will take place of 4 column. and in this way they will be centered */}
+          <div className="col-md-4">
+            <Newsitem title="my title" discription="my discription" />
+          </div>
+          <div className="col-md-4">
+            <Newsitem title="my title" discription="my discription" />
+          </div>
+          <div className="col-md-4">
+            <Newsitem title="my title" discription="my discription" />
+          </div>
         </div>
-      
-    )
+      </div>
+    );
   }
 }
--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -2,8 +2,22 @@ import React, { Component } from 'react'
 
 export default class Newsitem extends Component {
   render() {
+
+    // used the concept of destructuring the array here...in which this.props is an object and in that object title and discriminator is getting pulled away
+    let {title,discription} = this.props
+
     return (
-      <div>this is a news item2</div>
+
+                                    // i made this style as a js object then i write the object in it directly so inserted {} once again. wrote key directly and value in a double inverted comma
+      <div><div className="card" style={{width: "18rem"}}>
+      <img src="https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/E3CC/production/_128761385_brook.jpg" className="card-img-top" alt="..."/>
+      <div className="card-body">
+      {/* here title and discription is getting pulled from the above this .prop and wrote in this way in a curly bracke */}
+        <h5 className="card-title">{title} </h5>
+        <p className="card-text">{discription}</p>
+        <a href="/" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div></div>
     )
   }
 }
