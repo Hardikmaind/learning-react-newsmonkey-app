eba33dd50eb02ac39270c075200871787c72cafd
Hardikmaind <maindhardik@gmail.com>
2023-03-03 19:46:38 +0530

    vid-26 harry react course

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -1,16 +1,12 @@
+/* eslint-disable no-unused-vars */
 import React, { Component } from "react";
 
 export default class Newsitem extends Component {
 
 
-  // constructor(){
-  //   // must call  super constructor in derived class before accessing 'this' or returnning from derived constructor
-  //   super();
-  
-  //   console.log('hello i am constructor')
-  // }
 
   render() {
+    // this i how we use the props in the class bases components in the react
     // used the concept of destructuring the array here...in which this.props is an object and in that object title and discriminator is getting pulled away
     let { title, discription, imageUrl ,newsUrl} = this.props;
 
@@ -21,9 +17,9 @@ export default class Newsitem extends Component {
           <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             {/* here title and discription is getting pulled from the above this .prop and wrote in this way in a curly bracke */}
-            <h5 className="card-title">{title} </h5>
-            <p className="card-text">{discription}</p>
-            <a href="/newsdetail" className="btn btn-sm btn-primary">
+            <h5 className="card-title">{title}... </h5>
+            <p className="card-text">{discription}...</p>
+            <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">
               read more
             </a>
           </div>
