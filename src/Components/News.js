d5998f863b6b697051f51196401c45c6b410da1b
Hardikmaind <maindhardik@gmail.com>
2023-03-04 16:31:21 +0530

    vid-27 harry react vid (fetchapi)

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,60 +2,7 @@ import React, { Component } from "react";
 import Newsitem from "./Newsitem";
 
 export default class News extends Component {
-  //this is a property thats why didnt use the let var etc
-  // if the property name was just 'articles' then in this.state, you would write articles:this.articles to avod confusion i changes the name of the variable
-  news_articles = [
-    {
-      source: {
-        id: "bbc-sport",
-        name: "BBC Sport",
-      },
-      author: null,
-      title: "'Even without trying, England produce compelling Test'",
-      description:
-        "Having to chase 258 to win would not have been in England's plan after they asked New Zealand to follow on, but it sticks to their desire for compelling Test cricket, writes Stephan Shemilt.",
-      url: "http://www.bbc.co.uk/sport/cricket/64782397",
-      urlToImage:
-        "https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/E3CC/production/_128761385_brook.jpg",
-      publishedAt: "2023-02-27T11:37:25.128273Z",
-      content:
-        "England's desire to think outside the box resulted in Harry Brook taking his first Test wicket and New Zealand losing their last five wickets for 28 runs\r\nEven without trying, England have produced a… [+4965 chars]",
-    },
-    {
-      source: {
-        id: "espn-cric-info",
-        name: "ESPN Cric Info",
-      },
-      author: null,
-      title:
-        "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-      description:
-        "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-      url: "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-      urlToImage:
-        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-      publishedAt: "2020-04-27T11:41:47Z",
-      content:
-        "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]",
-    },
-    {
-      source: {
-        id: "espn-cric-info",
-        name: "ESPN Cric Info",
-      },
-      author: null,
-      title:
-        "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-      description:
-        "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-      url: "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-      urlToImage:
-        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-      publishedAt: "2020-03-30T15:26:05Z",
-      content:
-        "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]",
-    },
-  ];
+
 
   constructor() {
     // must call  super constructor in derived class before accessing 'this' or returnning from derived constructor
@@ -64,26 +11,43 @@ export default class News extends Component {
     console.log("hello i am constructor");
     this.state = {
       // this.news_articles property will get transferred into articles
-      articles: this.news_articles,
+      articles:[],
       loading: false,
     };
   }
 
+  // this will run after the render runs. the order is first constructor will run then render will run then componentdidmount() will mount will run  
+  async componentDidMount() {
+    let Url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=3d5dd63f2c134445a6d18d0607da4543"
+    // await funtions are only allowed inside the async function
+    let data = await fetch(Url)
+    let parsedData=await data.json()
+    console.log(parsedData);
+    this.setState({articles:parsedData.articles})
+
+
+
+    // this i have wriiten to show the order 
+    // console.log('cdm')
+  }
+
   render() {
+    // this i hvae written to show the order
+    // console.log('render')
     return (
       <div className="container my-3">
         <h2>newsmonkey top headlines</h2>
         <div className="row">
-        {/* here map is used to map the data to the above given article, its like loop */}
+          {/* here map is used to map the data to the above given article, its like loop */}
           {this.state.articles.map((element) => {
             return (
               <div className="col-md-4" key={element.url}>
-              {/*  key should be inside return  and in div not in newsitmen . key identifies uniquly everynews*/}
+                {/*  key should be inside return  and in div not in newsitmen . key identifies uniquly everynews*/}
                 {/* here you can see that i have passed the props in the Newsitem, u can see title ,discription,imageUrl */}
                 <Newsitem
-                // here slice will limit the title and discription to 45 and 88 chars resp.
-                  title={element.title.slice(0,45)}
-                  discription={element.description.slice(0,88)}
+                  // here slice will limit the title and discription to 45 and 88 chars resp.
+                  title={element.title?element.title.slice(0, 45):" "}
+                  discription={element.description?element.description.slice(0, 88):' '}
                   imageUrl={element.urlToImage}
                   newsUrl={element.url}
                 />
--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -14,7 +14,8 @@ export default class Newsitem extends Component {
       // i made this style as a js object then i write the object in it directly so inserted {} once again. wrote key directly and value in a double inverted comma
       <div className="my-3"> 
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+        {/* this says that, if image is not found then say, image not found ,or else show the imgUrl */}
+          <img src={!imageUrl?'image not found':imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             {/* here title and discription is getting pulled from the above this .prop and wrote in this way in a curly bracke */}
             <h5 className="card-title">{title}... </h5>
